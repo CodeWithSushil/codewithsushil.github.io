ea8386e0bf2f66f3169a3b1671709899c59945a5
sushilkumbhar77@gmail <sushilkumbhar77@gmail>
2023-02-15 13:04:37 +0530

    cws

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let date = new Date();
 let year = date.getFullYear();
-document.getElementById("year").innerHTML = year;
+console.log(year)
+document.querySelector("#year").innerHTML = year;
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
