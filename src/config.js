6e01463ab46c147da35097f02517d1bab97198a6
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-12-24 20:39:30 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
+import { env } from 'vite'
 
+const config = {
+  VITE_API_KEY: 'your_api_key_here',
+  VITE_BASE_URL: 'https://example.com',
+};
 
 console.log(import.meta.env.VITE_API_KEY);
 console.log(import.meta.env.VITE_BASE_URL);
