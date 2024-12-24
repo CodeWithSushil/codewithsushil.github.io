052a7cdf0a664bd9d94eb1f85ab7cbdd82dd38a7
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-12-24 20:50:24 +0530

    update

--- src/main.js
+++ src/main.js
@@ -4,3 +4,6 @@ import config from './config.js'
 document.querySelector('#app').innerHTML = `
 <h1 class="text-blue-600">Hello Sushil!</h1>
 `
+const url = import.meta.env.VITE_API_URL;
+
+alert(url);
