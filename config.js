c2d530aaebf7777e16c2a7dada1cef4dd8e6ec77
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-12-24 19:58:07 +0530

    add config file

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  API_KEY: 'your_api_key_here',
+  BASE_URL: 'https://example.com',
+};
+
+export default config;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import './style.css';
+import config from './main.js';
+
 document.querySelector('#app').innerHTML = `
 <h1 class="text-blue-600">Hello Sushil!</h1>
 `;
+
+console.log(import.meta.env.VITE_API_KEY);
+console.log(import.meta.env.VITE_BASE_URL);
+
+console.log(config.API_KEY);
+console.log(config.BASE_URL);
