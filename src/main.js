79a7f3d47e9964f4eef47587e02d3084776ed936
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-12-24 20:28:15 +0530

    add config file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-import './style.css';
-import config from './main.js';
+import './style.css'
 
 document.querySelector('#app').innerHTML = `
 <h1 class="text-blue-600">Hello Sushil!</h1>
-`;
-
+<script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+<script>eruda.init()</script>
+`
 console.log(import.meta.env.VITE_API_KEY);
 console.log(import.meta.env.VITE_BASE_URL);
-
-console.log(config.API_KEY);
-console.log(config.BASE_URL);
